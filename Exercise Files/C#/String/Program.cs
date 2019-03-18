01a3e6ccb3458c0f557e3d2a0c75df5b75083289
alinaqvii <wajahat.naqvi21@gmail.com>
2019-03-18 23:52:37 +0500

    C# Basics-String

--- Exercise Files/C#/String/Program.cs
+++ Exercise Files/C#/String/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace String
 {
@@ -6,38 +7,70 @@ namespace String
     {
         static void Main(string[] args)
         {
-            var fullName = "Ali Naqvi ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("UpperCase: '{0}'", fullName.Trim().ToUpper());
+            // var fullName = "Ali Naqvi ";
+            // Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            // Console.WriteLine("UpperCase: '{0}'", fullName.Trim().ToUpper());
 
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            System.Console.WriteLine("First Name: " + firstName);
-            System.Console.WriteLine("Last Name: " + lastName);
+            // var index = fullName.IndexOf(' ');
+            // var firstName = fullName.Substring(0, index);
+            // var lastName = fullName.Substring(index + 1);
+            // System.Console.WriteLine("First Name: " + firstName);
+            // System.Console.WriteLine("Last Name: " + lastName);
 
-            //Using Split Method
-            var names = fullName.Split(' ');
-            System.Console.WriteLine("First Name: " + names[0]);
-            System.Console.WriteLine("Last Name: " + names[1]);
+            // //Using Split Method
+            // var names = fullName.Split(' ');
+            // System.Console.WriteLine("First Name: " + names[0]);
+            // System.Console.WriteLine("Last Name: " + names[1]);
 
-            Console.WriteLine(fullName.Replace("Ali", "ALI"));
+            // Console.WriteLine(fullName.Replace("Ali", "ALI"));
 
-            //To check whitespaces
+            // //To check whitespaces
 
-            //if (String.IsNullOrWhiteSpace(" "))
+            // //if (String.IsNullOrWhiteSpace(" "))
 
-            // String to Int
+            // // String to Int
 
-            var str = "25";
-            var age = Convert.ToInt32(str);
-            System.Console.WriteLine(age);
+            // var str = "25";
+            // var age = Convert.ToInt32(str);
+            // System.Console.WriteLine(age);
 
-            //Int to String
+            // //Int to String
 
-            float x = 25.95f;
-            System.Console.WriteLine(x.ToString("C")); 
+            // float x = 25.95f;
+            // System.Console.WriteLine(x.ToString("C")); 
+            
+            //Summarising Text
 
+            var sentence = "This is very very very very  very long text";
+            var summary = SummerizeText(sentence, 35);
+            System.Console.WriteLine(summary);           
+            
+        }
+
+        static string SummerizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+            else
+            {
+                var words = text.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                    {
+                        break;
+                    }
+                }
+                return System.String.Join(" ", summaryWords) + "...";
+            }
         }
     }
 }
---
+++ Exercise Files/C#/String/String Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-',10)
+                   .AppendLine()
+                   .Append("Header")
+                   .AppendLine()
+                   .Append('-',10)
+                   .Replace('-', '+')
+                   .Remove(0, 10)
+                   .Insert(0, new string('-', 10));
+                   
+            System.Console.WriteLine(builder);
+
+        }
+    }
+}
