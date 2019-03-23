2b1b9db2dcdeef5a057a9b9f9849503a85513f5b
alinaqvii <wajahat.naqvi21@gmail.com>
2019-03-23 14:33:34 +0500

    Encapsulation

---
+++ Exercise Files/OOP/Encapsulation/Computer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExamples
+{
+    class Computer
+    {
+
+        protected string _name = "Unknown";
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        public Computer(string name)
+        {
+            this.name = name;
+        }
+
+    }
+}
---
+++ Exercise Files/OOP/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOPExamples;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var comp = new Computer("Generic Computer");
+        
+        Console.WriteLine("Computer Type - " + comp.GetType());
+
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("Is computer - " + isComputer);
+        Console.WriteLine("Computer name - " + comp.name);
+
+        Console.Read();
+
+    }
+}
+
