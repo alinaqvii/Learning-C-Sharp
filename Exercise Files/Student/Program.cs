770a9b33ff434d170c891f7ca2dd754425fe4d28
alinaqvii <wajahat.naqvi21@gmail.com>
2019-04-11 08:57:27 +0500

    studentEnolment

--- Exercise Files/Student/Program.cs
+++ Exercise Files/Student/Program.cs
@@ -1,55 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
 
 namespace Student
 {
     class Program
     {
-        enum Title
-        {
-            Maths,
-            Chemistry,
-            Physics,
-            Computer
-        }
         static void Main(string[] args)
         {
-            List<Student> student = new List<Student>
+            var student1 = new Student(1, "Ali", "Naqvi", 22);
+            var student2 = new Student(2, "Muhammad", "Ali", 21);
+            var student3 = new Student(3, "Nasir", "Hussain", 29);
+
+            var course = new Course("Programming with C#")
             {
-                new Student {ID = 1, Name = "Ali", FName = "Muhammad", DOB = 02011990},
-                new Student {ID = 2, Name = "Hussain", FName = "Ali", DOB = 02011989},
-                new Student {ID = 3, Name = "Salman", FName = "Ahmed", DOB = 12121987}
+                Students = new Collection<Student> { student1, student2, student3 }
             };
 
-            List<Course> course = new List<Course>
-            {
-                new Course{CourseID = 1, Title = "Maths"},
-                new Course{CourseID = 2, Title = "Computer"},
-                new Course{CourseID = 3, Title = "Physics"}
-            };
+
+            Console.WriteLine(course);
         }
     }
 
-    class Student
+    public class Person
     {
-        public int ID;
-        public string Name;
-        public string FName;
-        public int DOB;
-
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private string FName { get; set; }
+        private int PersonAge { get; set; }
+        public Person(int id, string name, string fatherName, int age)
+        {
+            ID = id;
+            Name = name; 
+            FName = fatherName;
+            PersonAge = age;
+        }
     }
 
-    class Course
+    public class Student : Person
     {
-        public int CourseID;
-        public string Title;
+        public static int EnrolledCount;
 
+        public Student(int id, string name, string fatherName, int age)
+            : base(id, name, fatherName, age)
+        {
+            EnrolledCount++;
+        }
+       
     }
-    
 
-    // class StudentCourse : Student
-    // {
-    //     public int StudentID = ID;
+    public class Course
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public string CourseName { get; set; }
 
-    // }
+        public Course(string name)
+        {
+            CourseName = name;
+            Students = new List<Student>();
+        }
+
+        public override string ToString()
+        {
+            return "The " + CourseName + " course contains " + Students.Count() + " student(s)";
+        }
+    }
 }
+
+
+
+
